850f8b9476f50c26209658fdfee4ba155750d332
nick wuerz <nawuerz@gmail.com>
2020-01-16 14:49:27 -0600

    psuedo coded idea for javascript layout

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+// save applicable elements as var(s) to be used later..
+
 var goBack = document.querySelector("goBack");
 var clearScores = document.querySelector("clearScores");
+
+// listen for button activity on these elements to prompt next event
+
+//when start quiz is hit, start the timer and prompt the first set of questions and answers (traverse dom to access and replace elements)
+
+//when answer is selected, display the result and deduct time if needed
+
+// repeat for each set of questinos
+
+// display result to user and save to the browser
+
+//program the "clear results button"
+
+//style page and add to your responsive portfolio
+
